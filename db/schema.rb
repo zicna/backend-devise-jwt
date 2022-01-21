73a442a15c2e3bad4a6ef70109ec3bd68673d711
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-01-21 16:34:51 -0600

    create model 'jwt_denylist.rb' and migration 'CreateJwtDenylist'

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_214230) do
+ActiveRecord::Schema.define(version: 2022_01_21_222741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["jti"], name: "index_jwt_denylists_on_jti"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
